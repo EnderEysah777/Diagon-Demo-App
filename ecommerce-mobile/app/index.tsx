7d677a8a3034737b03a8d5d9628e265bd68fa455
John Doe <johndoe@email.com>
2025-08-13 08:34:43 -0400

    Product List Structure

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,13 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import products from '../assets/products.json';
+import ProductListItem from "../components/ProductListItem"; 
+
+
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Home Screen</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+      <FlatList 
+         data = {products}
+         renderItem = {({item}) => <ProductListItem product={item}/>}
+      />
   );
 }
 
